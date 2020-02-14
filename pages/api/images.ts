15b923b6b4b20bba904e713c5e3096f7af47c461
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2020-02-14 13:34:24 +0100

    fix: another attempt to fix prod

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -2,14 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import fs from 'fs';
 import { ImagesAPIResponse } from "../../types/api";
 import { resolve, join } from 'path';
-import getConfig from 'next/config';
 
-const { serverRuntimeConfig } = getConfig();
-
-const imagesPath = join(serverRuntimeConfig["PROJECT_ROOT"], "public", "images")
+const imagesPath = join(process.cwd(), "public", "images")
 
 const ImagesAPIEndpoint = async (_: NextApiRequest, response: NextApiResponse) => {
-    console.log(resolve("public/images"))
     const imagesDirectory = fs.readdirSync(imagesPath);
     const responseData: ImagesAPIResponse = imagesDirectory.map(file => ({
         url: `/images/${encodeURI(file)}`,
