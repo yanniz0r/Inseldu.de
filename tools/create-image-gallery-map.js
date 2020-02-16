0200a800450fd8e3eec2cbffe26864b176e31943
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2020-02-16 20:51:48 +0100

    feat: subdirectory for gallery

--- tools/create-image-gallery-map.js
+++ tools/create-image-gallery-map.js
@@ -1,13 +1,13 @@
 const { readdirSync, writeFileSync } = require("fs");
 const { join } = require("path");
 
-const galleryPath = join(__dirname, "..", "public", "images");
+const galleryPath = join(__dirname, "..", "public", "images", "gallery");
 const galleryMapPath = join(__dirname, "..", "data", "gallery-data.json");
 
 const galleryDirectory = readdirSync(galleryPath);
 
 const galleryMap = galleryDirectory.map(file => ({
-    url: `/images/${file}`,
+    url: `/images/gallery/${encodeURI(file)}`,
     description: file.replace(/\..+/g, "")
 }));
 
