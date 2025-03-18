16cf69a633f37c2881023fd822e8269fc6c317fd
Yannic Inselmann <yannic.inselmann@gmail.com>
2025-03-18 11:57:50 +0100

    feat: update project cards layout and add new project "Invocraft"

--- components/pages/index/projects.tsx
+++ components/pages/index/projects.tsx
@@ -13,7 +13,7 @@ const Projects: FC<ProjectsProps> = (props) => {
       <h2 className="text-center text-4xl font-extrabold text-slate-900 dark:text-slate-50">
         Projects
       </h2>
-      <div className="mt-10 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="mt-10 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">
         <ProjectCard
           title={t("projects.partymate.title")}
           type={t("projects.partymate.type")}
@@ -28,8 +28,8 @@ const Projects: FC<ProjectsProps> = (props) => {
           title={t("projects.binckebanck.title")}
           type={t("projects.binckebanck.type")}
           url="https://binckebanck.com"
-          imageWidth={600}
-          imageHeight={470}
+          imageWidth={800}
+          imageHeight={570}
           imageURL="/binckebanck.png"
         >
           <Paragraphs text={t("projects.binckebanck.description")} />
@@ -38,8 +38,8 @@ const Projects: FC<ProjectsProps> = (props) => {
           title={t("projects.memoryMachine.title")}
           type={t("projects.memoryMachine.type")}
           url="https://memorymachine.de"
-          imageWidth={600}
-          imageHeight={350}
+          imageWidth={800}
+          imageHeight={450}
           imageURL="/memory-machine.png"
         >
           <Paragraphs text={t("projects.memoryMachine.description")} />
@@ -50,8 +50,8 @@ const Projects: FC<ProjectsProps> = (props) => {
           githubURL="https://github.com/yanniz0r/sonq"
           url="https://sonq.de"
           imageURL="/sonq.png"
-          imageWidth={600}
-          imageHeight={400}
+          imageWidth={800}
+          imageHeight={600}
         >
           <Paragraphs text={t("projects.sonq.description")} />
         </ProjectCard>
@@ -59,8 +59,8 @@ const Projects: FC<ProjectsProps> = (props) => {
           title={t("projects.reverbOrdermanager.title")}
           type={t("projects.reverbOrdermanager.type")}
           imageURL="/reverb-order-manager.png"
-          imageWidth={600}
-          imageHeight={400}
+          imageWidth={800}
+          imageHeight={600}
         >
           <Paragraphs text={t("projects.reverbOrdermanager.description")} />
         </ProjectCard>
@@ -68,10 +68,21 @@ const Projects: FC<ProjectsProps> = (props) => {
           title={t("projects.spicyChances.title")}
           type={t("projects.spicyChances.type")}
           imageURL="/spicy-chances.png"
-          imageWidth={600}
-          imageHeight={400}
+          url="https://spicychances.de"
+          imageWidth={800}
+          imageHeight={600}
         >
           <Paragraphs text={t("projects.spicyChances.description")} />
+        </ProjectCard>
+        <ProjectCard
+          title={t("projects.invocraft.title")}
+          type={t("projects.invocraft.type")}
+          imageURL="/invocraft.png"
+          url="https://preview.invocraft.de"
+          imageWidth={800}
+          imageHeight={600}
+        >
+          <Paragraphs text={t("projects.invocraft.description")} />
         </ProjectCard>
       </div>
     </Section>
